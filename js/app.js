02f5f94f5e0d7f1cf6a088929c92656a4543a882
João <joaovictorleal080415@gmail.com>
2022-05-03 00:54:35 -0300

    it

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@
     const descricao = document.querySelector('#contentassunt'); 
     const enviar = document.querySelector('#enviarResposta')
     const answers = document.getElementById('answersTopics')
+    let x = 0 
     teste.addEventListener('click', ()=> {
 
         divisoria.classList.remove('contentresumo');    
@@ -27,23 +28,46 @@
 
     enviar.addEventListener('click', e => {
         e.preventDefault(); 
-            
+        
+
+
             
         if(assunto.value !== '' && descricao.value !== ''){
-               
+              
            let assunts =  document.createElement('h3')
             let paragraph = document.createElement('p'); 
             let expan = document.createElement('span'); 
+            let imagemUm = document.createElement('img'); 
+            let imagemDois = document.createElement('img'); 
+            let expanLike = document.createElement('span'); 
+            let expanResposta = document.createElement('span'); 
             expan.classList.add('expansaoDinamica')
             assunts.classList.add('subtituloDinamico'); 
             paragraph.classList.add('paragrafoExpansao'); 
+            expan.innerHTML = 'João Victor Leal Santos Cardoso'
+            expan.classList.add('expansaoTopicos')
+            imagemUm.src = '../assets/maisinformações.svg'; 
+            imagemDois.src = '../assets/favoritar.svg' ; 
+            expanLike.classList.add('like'); 
+            expanResposta.classList.add('like'); 
+            expanLike.innerHTML = '1 like'; 
+            expanResposta.innerHTML = '3 resposta'; 
+            
+            
+            
+            
+            
             assunts.innerHTML = assunto.value;
             paragraph.innerHTML = descricao.value; 
             
             
             answers.appendChild(assunts); 
             answers.appendChild(paragraph); 
-
+            answers.insertBefore(expan, assunts.nextSibling); 
+            answers.appendChild(imagemUm)
+            answers.appendChild(imagemDois)
+            answers.appendChild(expanLike)
+            answers.appendChild(expanResposta)
             
 
         }    
