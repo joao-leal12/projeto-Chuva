5162c8804f6c48bb2ed712cfa479a5ad357b5681
João <joaovictorleal080415@gmail.com>
2022-05-03 00:39:22 -0300

    Novo commit

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@
     const divisoria = document.querySelector('#divDinamica'); 
     const cadastro = document.querySelector('#CadastroDuvidas'); 
     const CriarTopicos = document.querySelector('#cadastrarTopico'); 
-    const criador = document.querySelector('#criador')
-
-        
+    const criador = document.querySelector('#criador');
+    const assunto = document.querySelector('#valueInput');
+    const descricao = document.querySelector('#contentassunt'); 
+    const enviar = document.querySelector('#enviarResposta')
+    const answers = document.getElementById('answersTopics')
     teste.addEventListener('click', ()=> {
 
         divisoria.classList.remove('contentresumo');    
@@ -20,4 +22,34 @@
         criador.classList.toggle('displayNone'); 
 
     }); 
+ 
+    
 
+    enviar.addEventListener('click', e => {
+        e.preventDefault(); 
+            
+            
+        if(assunto.value !== '' && descricao.value !== ''){
+               
+           let assunts =  document.createElement('h3')
+            let paragraph = document.createElement('p'); 
+            let expan = document.createElement('span'); 
+            expan.classList.add('expansaoDinamica')
+            assunts.classList.add('subtituloDinamico'); 
+            paragraph.classList.add('paragrafoExpansao'); 
+            assunts.innerHTML = assunto.value;
+            paragraph.innerHTML = descricao.value; 
+            
+            
+            answers.appendChild(assunts); 
+            answers.appendChild(paragraph); 
+
+            
+
+        }    
+
+        assunto.value = ''; 
+        descricao.value = ''; 
+            
+
+    });
